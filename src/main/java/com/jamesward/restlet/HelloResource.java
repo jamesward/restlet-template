7060be451696bad22d0f1bd747d878374d7d23c7
James Ward <james@jamesward.org>
2012-02-06 10:17:49 -0700

    ssl check

--- src/main/java/com/jamesward/restlet/HelloResource.java
+++ src/main/java/com/jamesward/restlet/HelloResource.java
@@ -1,5 +1,6 @@
 package com.jamesward.restlet;
 
+import org.restlet.data.Form;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
@@ -7,8 +8,16 @@ public class HelloResource extends ServerResource {
 
     @Get
     public String represent() {
+        Form requestHeaders = (Form) this.getRequest().getAttributes().get("org.restlet.http.headers");
+
+        boolean secure = false;
+        if (requestHeaders.getValues("x-forwarded-proto") != null) {
+            secure = requestHeaders.getValues("x-forwarded-proto").contains("https");
+        }
+
+        System.out.println("secure = " + secure);
+
         return "hello, world";
     }
 
 }
-
